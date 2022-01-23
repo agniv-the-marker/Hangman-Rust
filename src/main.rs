538795aed2a0b8c4786eddd2df79b373de3b6a3b
agniv-the-marker <54725025+agniv-the-marker@users.noreply.github.com>
2022-01-22 21:04:06 -0800

    Overhaul

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,78 @@
-use rand::Rng;
-use std::fs;
-use std::io::{self, Read};
+use help::{start, cur_word, input, remove_duplicates, end};
 fn main() {
     print!("\x1B[2J\x1B[1;1H");
-    let word = get_word();
-    let mut guessed = false;
-    let splitletters = word.split("");
-    let letters = remove_duplicates(splitletters.collect::<Vec<&str>>());
-    let mut rguessedletters: Vec<String> = vec!["".to_owned()];
-    let mut wguessedletters: Vec<String> = vec![];
-    let mut tries = 0;
-    while wguessedletters.len() < 10 {
-        let mut ask = "You are now guessing: ".to_owned();
-        let mut cur = "".to_owned();
-        for l in word.split("") {
-            cur = if !(rguessedletters.contains(&l.to_owned())) {
-                [cur, "_".to_owned()].join("")
-            } else {
-                [cur, l.to_owned()].join("")
-            };
-        }
-        ask = ask + &cur.to_owned();
-        let mut guessedletters = "".to_owned();
-        for r in &rguessedletters {
-            guessedletters = [guessedletters, r.to_owned(), ", ".to_owned()].join("");
-        }
-        for w in &wguessedletters {
-            guessedletters = [guessedletters, w.to_owned(), ", ".to_owned()].join("");
-        }
-        if guessedletters.len() > 2 {
-            guessedletters = guessedletters[1..guessedletters.len()-2].to_owned();
-        }
-        println!("{}\nYou have guessed these letter(s):\n{}\nYou have {} more chance(s).", ask, guessedletters, 10-wguessedletters.len());
-        print!("Guess a letter or the word: ");
-        io::Write::flush(&mut io::stdout()).expect("flush failed!");
-        let mut guess = String::new();
-        match io::stdin().read_line(&mut guess) {
-            Ok(_) => { },
-            Err(err) => println!("Could not parse input: {}", err)
-        }    
-        guess = guess.trim().to_owned();
-        if guess == word {
-            guessed = true;
-            break;  
-        }
-        if !(guess.len() == 1) | !("abcdefghijklmnopqrstuvwxyz").contains(&guess) {
-            println!("{} isn't a letter.\n", guess);
-            continue;
-        }
-        if wguessedletters.contains(&guess) | rguessedletters.contains(&guess) {
-            println!("You already guessed {}!\n", guess);
-            continue;
-        }
-        tries = tries + 1;
-        if letters.contains(&&*guess) {
-            println!("{} was a letter!\n", guess);
-            rguessedletters.push(guess);
-            println!("{}", letters[0]);
-            if rguessedletters.len() == letters.len() {
-                guessed = true;
+
+    let (games, words, guesses) = start(); 
+
+    let mut word;
+    let mut guessed;
+    let mut letters;
+    let mut r_guessed_letters;
+    let mut w_guessed_letters;
+    let mut tries;
+    let mut ask;
+    let mut used;
+    let mut chances;
+    let mut guess;
+
+    for g in 0..games {
+
+        println!("Game {}!", g+1);
+        word = words.get(g).unwrap();
+        guessed = false;  
+        letters = remove_duplicates(word.split("").collect::<Vec<&str>>());
+        r_guessed_letters = vec!["".to_owned()];
+        w_guessed_letters = vec![];
+        tries = 0;
+        ask = format!("You are now guessing: {}", "_".repeat(word.len()));
+        used = "You have guessed these letter(s):\n".to_owned();
+        chances = format!("You have {} more chance(s).\nGuess a letter or the word:", guesses-w_guessed_letters.len());
+
+        while w_guessed_letters.len() < guesses {
+            print!("{}\n{}{} ", ask, used, chances);
+            guess = input().trim().to_owned();
+            if guess == "EXIT" {
+                end();
                 break;
             }
+            if guess == *word {
+                guessed = true;
+                break;  
+            }
+            if (guess.len() != 1) | !("abcdefghijklmnopqrstuvwxyz").contains(&guess) {
+                println!("{} isn't a letter.\n", guess);
+                continue;
+            }
+            if w_guessed_letters.contains(&guess) | r_guessed_letters.contains(&guess) {
+                println!("You already guessed {}!\n", guess);
+                continue;
+            }
+            tries += 1;
+            if used == *"You have guessed these letter(s):\n" {
+                used = format!("{}{}", used, guess);
+            } else {
+                used = format!("{}{}{}", used, ", ", guess);
+            }
+            if letters.contains(&guess.as_str()) {
+                println!("{} was a letter!", guess);
+                r_guessed_letters.push(guess);
+                ask = cur_word(word, &r_guessed_letters);
+                println!("{}", letters[0]);
+                if r_guessed_letters.len() == letters.len() {
+                    guessed = true;
+                    break;
+                }
+            } else {
+                println!("Nope, {} isn't in the word!\n", guess);
+                w_guessed_letters.push(guess);
+            }
+            chances = format!("\nYou have {} more chance(s).\nGuess a letter or the word:", guesses-w_guessed_letters.len());
+        }
+        if guessed {
+            println!("You did it in {} tries!\nThe word was {}.\n\n", tries, word);
         } else {
-            println!("Nope, {} isn't in the word!\n", guess);
-            wguessedletters.push(guess);
+            println!("Better luck next time!\nThe word was {}.\n", word);
         }
     }
-    if guessed {
-        println!("You did it in {} tries!\nThe word was {}.", tries, word);
-    } else {
-        println!("Better luck next time!\nThe word was {}.", word);
-    }
-    println!("Press enter to exit");
-
-    let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer).ok();
+    end();
 }
-
-
-fn get_word() -> String {
-    let temp = fs::read_to_string("src/words.txt").expect("Something went wrong reading the file");
-    let contents = temp.trim().split("\n");
-    let vec = contents.collect::<Vec<&str>>();
-    let mut rng = rand::thread_rng();
-    vec[rng.gen_range(0..vec.len())].trim().to_owned()
-}
-
-fn remove_duplicates(mut arr : Vec<&str>) -> Vec<&str> {
-    arr.sort();
-    arr.dedup();
-    arr
-}
-
