872ed605ca224ad6cf234356e34fcc7b1fdbe44c
agniv-the-marker <54725025+agniv-the-marker@users.noreply.github.com>
2022-01-29 17:27:46 -0800

    Add files via upload

--- src/help.rs
+++ src/help.rs
@@ -33,6 +33,14 @@ pub fn cur_word(word: &str, r_guessed_letters: &[String]) -> String {
     ask
 }
 
+pub fn get_word() -> String {
+    let temp = fs::read_to_string("src/words.txt").expect("Something went wrong reading the file");
+    let contents = temp.trim().split("\n");
+    let vec = contents.collect::<Vec<&str>>();
+    let mut rng = rand::thread_rng();
+    vec[rng.gen_range(0..vec.len())].trim().to_owned()
+}
+
 fn get_words(words: usize) -> Vec<String> {
     let temp = fs::read_to_string("src/words.txt").expect("Something went wrong reading the file");
     let contents = temp.trim().split('\n');
@@ -44,6 +52,7 @@ fn get_words(words: usize) -> Vec<String> {
         let word = vec[i].trim().to_owned();
         vec.remove(i);
         w.push(word);
+        
     }
     w
 }
